9a3b6ec4c45cac6e88880900208059194245238b
BathTubTheGreat <yilyanel@nycstudents.net>
2026-01-28 10:16:10 -0500

    day 2
    square and triangle

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-cfyghjcfgcghjgchghjk
+import turtle
+from turtle import *
+t = Turtle()
+"""
+def rectangle():
+    t.forward(125)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(125)
+    t.right(90)
+    t.forward(100)
+rectangle()
+"""
+
+def tri(x):
+    t.forward(x)
+    t.right(120)
+    t.forward(x)
+    t.right(120)
+    t.forward(x)
+tri(90)
+
+Turtle.done()
