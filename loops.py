5b843f4f89b8f2c1aa9c3544edd31bfd4717c3c4
BathTubTheGreat <yilyanel@nycstudents.net>
2026-01-30 10:11:02 -0500

    finished it all

--- loops.py
+++ loops.py
@@ -1,20 +1,42 @@
 import turtle
 from turtle import *
 t = Turtle()
-t.speed(10)
-
+t.speed(0)
+"""
 def square():
     x = 5
     y = 90
     turn = 5
     for i in range(60):
         t.forward(x)
-        t.right(y)
+        t.left(y)
         t.forward(x)
-        t.right(y)
+        t.left(y)
         t.forward(x)
-        t.right(y)
+        t.left(y)
+        t.forward(x)
+        t.left(y)
         t.right(turn)
         x = x + 5
 square()
+"""
+
+def star():
+    x = 5
+    y = 144
+    turn = 5
+    for i in range(60):
+        t.forward(x)
+        t.left(y)
+        t.forward(x)
+        t.left(y)
+        t.forward(x)
+        t.left(y)
+        t.forward(x)
+        t.left(y)
+        t.forward(x)
+        t.left(y)
+        t.right(turn)
+        x = x + 5
+star()
 t.done()
